effd590cbedcf765be647a25574b48930e2ca2e3
skurtz97 <seankurtz75@gmail.com>
2022-03-04 06:54:11 -0600

    Fixed cors bug

--- server/middleware.go
+++ server/middleware.go
@@ -5,9 +5,6 @@ import "net/http"
 func middlewareCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "http://localhost")
-		if r.Method == "OPTIONS" {
-			return
-		}
 		next.ServeHTTP(w, r)
 	})
 }
