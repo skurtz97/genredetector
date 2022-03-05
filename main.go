86624797105fcea6fefb297633b3e82872419f76
skurtz97 <seankurtz75@gmail.com>
2022-03-04 23:25:05 -0600

    PORT for development env

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ var ErrParseForm = errors.New("error parsing incoming request form")
 
 func main() {
 	port := os.Getenv("PORT")
+
 	s := server.NewServer("localhost:"+port, false)
 	log.Printf("listening on %s", s.Addr)
 	log.Fatal(s.ListenAndServe())
